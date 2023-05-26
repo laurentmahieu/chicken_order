0eced52f4c7c8c509539f099caeaf54bd9756cd5
laurent <laurent.mahieu@bisom.fr>
2023-05-26 22:54:35 +0200

    init MyInput component

---
+++ src/components/MyInput.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="myInput">
+    <label for="name">{{ label }}</label>
+
+    <input :type="type" name="name" required v-model="modelValue" />
+  </div>
+  <p></p>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "MyInput",
+
+  props: {
+    label: String,
+    type: { String, default: "number" },
+  },
+
+  data() {
+    return {
+      modelValue: "",
+    };
+  },
+});
+</script>
+
+<style scoped>
+label {
+  color: aliceblue;
+}
+input {
+  border: 1px solid aliceblue;
+  border-radius: 5px;
+  padding: 10px;
+  background-color: black;
+  color: aliceblue;
+}
+
+input:focus {
+  border: 1px solid orange;
+}
+.myInput {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  padding: 4px;
+}
+</style>
