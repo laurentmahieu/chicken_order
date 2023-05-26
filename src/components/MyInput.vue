bf38b136f85e6184129a295dd5307d5856ed5bd3
laurent <laurent.mahieu@bisom.fr>
2023-05-27 00:04:13 +0200

    update My input display, for rules messages

--- src/components/MyInput.vue
+++ src/components/MyInput.vue
@@ -2,9 +2,18 @@
   <div class="myInput">
     <label for="name">{{ label }}</label>
 
-    <input :type="type" name="name" required v-model="modelValue" />
+    <input
+      :type="type"
+      :name="name"
+      required
+      v-model="value"
+      :class="fieldOk ? 'borderRed' : ''"
+      @blur="$emit('blurInput')"
+    />
+    <div class="requireMessage">
+      <span v-if="fieldOk"> Ce champs est requis </span>
+    </div>
   </div>
-  <p></p>
 </template>
 
 <script lang="ts">
@@ -15,13 +24,31 @@ export default defineComponent({
 
   props: {
     label: String,
+    name: String,
+    required: Boolean,
+    showRequireMessage: Boolean,
     type: { String, default: "number" },
+    modelValue: String,
   },
 
   data() {
     return {
-      modelValue: "",
+      // modelValue: "",
     };
+  },
+
+  computed: {
+    value: {
+      get() {
+        return this.modelValue;
+      },
+      set(newValue: string) {
+        this.$emit("update:modelValue", newValue);
+      },
+    },
+    fieldOk() {
+      return this.required && this.showRequireMessage && !this.modelValue;
+    },
   },
 });
 </script>
@@ -29,13 +56,23 @@ export default defineComponent({
 <style scoped>
 label {
   color: aliceblue;
+  background-color: black;
+  margin-bottom: -4px;
+  margin-left: 5px;
+  padding: 0 4px;
+  width: max-content;
+  z-index: 2;
+  font-size: small;
 }
 input {
   border: 1px solid aliceblue;
   border-radius: 5px;
-  padding: 10px;
+  padding: 15px;
   background-color: black;
   color: aliceblue;
+}
+.borderRed {
+  border: 1px solid #a83234;
 }
 
 input:focus {
@@ -46,5 +83,11 @@ input:focus {
   display: flex;
   flex-direction: column;
   padding: 4px;
+}
+
+.requireMessage {
+  color: #a83234;
+  font-size: smaller;
+  height: 15px;
 }
 </style>
