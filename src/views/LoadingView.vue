6deff386a79c8da4534ba7c46117118a8176afbe
laurent <laurent.mahieu@bisom.fr>
2023-05-26 21:44:52 +0200

    create LoadingView

--- src/views/LoadingView.vue
+++ src/views/LoadingView.vue
@@ -1,3 +1,51 @@
 <template>
-  <h1>This is an LoadingView</h1>
+  <div id="loadingView">
+    <img src="/android-chrome-384x384.png" alt="alt" id="loadingViewImg" />
+  </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "LoadingView",
+
+  mounted() {
+    setTimeout(() => {
+      this.closeLoader();
+    }, 3000);
+  },
+
+  methods: {
+    closeLoader() {
+      this.$router.push("/home");
+    },
+  },
+});
+</script>
+
+<style scoped>
+@keyframes rotation {
+  from {
+    transform: rotateY(0deg);
+  }
+  to {
+    transform: rotateY(359deg);
+  }
+}
+
+#loadingView {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+}
+
+#loadingViewImg {
+  width: 200px;
+  height: 200px;
+  animation: rotation 3s infinite linear;
+}
+</style>
