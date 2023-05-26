c3d4c575d0b4fc5a7bb0066e16638bb4ece15d15
laurent <laurent.mahieu@bisom.fr>
2023-05-27 00:03:28 +0200

    init MyButton

---
+++ src/components/MyButton.vue
@@ -0,0 +1,34 @@
+<template>
+  <button
+    :style="`border: 1px solid ${color}; color: ${color}; background-color: ${bgColor};`"
+  >
+    {{ msg }}
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "MyButton",
+
+  props: {
+    msg: String,
+    color: { String, default: "orange" },
+    bgColor: { String, default: "aliceblue" },
+  },
+});
+</script>
+
+<style scoped>
+button {
+  width: 120px;
+  margin: 10px;
+  padding: 10px;
+  text-transform: uppercase;
+  border: none;
+  border-radius: 10px;
+  border: 1px solid orange;
+  color: orange;
+}
+</style>
