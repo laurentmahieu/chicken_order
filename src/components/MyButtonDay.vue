66780c7b9576facdad8370caf07b5a07cee0d720
laurent <laurent.mahieu@bisom.fr>
2023-06-05 22:00:25 +0200

    create component MyButtonDay

---
+++ src/components/MyButtonDay.vue
@@ -0,0 +1,36 @@
+<template>
+  <button
+    :style="`border: 1px solid ${color}; color: ${color}; background-color: ${bgColor};`"
+  >
+    <h4>{{ day }}</h4>
+    <p>{{ msg }}</p>
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "MyButtonDay",
+
+  props: {
+    day: String,
+    msg: String,
+    color: { String, default: "navy" },
+    bgColor: { String, default: "aliceblue" },
+  },
+});
+</script>
+
+<style scoped>
+button {
+  width: 120px;
+  margin: 10px;
+  padding: 10px;
+  text-transform: uppercase;
+  border: none;
+  border-radius: 10px;
+  border: 1px solid navy;
+  color: navy;
+}
+</style>
