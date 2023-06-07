2e3a21d18740c2cef75e1f8a5296c844862264fc
laurent <laurent.mahieu@bisom.fr>
2023-06-07 16:44:52 +0200

    install vuetify

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,19 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 
+// Vuetify
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
 import "./assets/main.css";
 
 const app = createApp(App);
 const pinia = createPinia();
+const vuetify = createVuetify({
+  components,
+  directives,
+});
 
-app.use(router);
-app.use(pinia);
-
-app.mount("#app");
+app.use(router).use(pinia).use(vuetify).mount("#app");
