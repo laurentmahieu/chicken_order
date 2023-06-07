7b2ef56a4b6e51e8f79f0f4c176fd3db8695fcb5
laurent <laurent.mahieu@bisom.fr>
2023-06-07 22:54:16 +0200

    create const articles

---
+++ src/const/articles.ts
@@ -0,0 +1,24 @@
+const ARTICLES = [
+  {
+    name: "Poulet Lick",
+    subtitle: "",
+    price: "n €",
+  },
+  {
+    name: "Poulet Moyen",
+    subtitle: "1,4kg avant cuisson",
+    price: "n €",
+  },
+  {
+    name: "Petit Poulet",
+    subtitle: "1,2kg avant cuisson",
+    price: "n €",
+  },
+  {
+    name: "Barquette Pomme de Terre",
+    subtitle: "",
+    price: "n €",
+  },
+];
+
+export default ARTICLES;
